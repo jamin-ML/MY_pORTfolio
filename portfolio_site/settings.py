78d967e12f440f1dac8d5c58fe685eba33e5a504
jamin-ML <peterping725@gmail.comgit config --global user.name jamin-MLgit config --global user.email peterping725@gmail.com>
2026-04-03 21:11:43 +0300

    Db

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,13 +63,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-        
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
-
 AUTH_PASSWORD_VALIDATORS = []
 
 MESSAGE_TAGS = {
